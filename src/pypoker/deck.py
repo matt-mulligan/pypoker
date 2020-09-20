bb5db29943237e42f168861c25b8e2009c8e5195
Matt Mulligan <matt@mattmulligan.net>
2020-09-20 18:36:47 +1000

    updated card class as dataclass added type annotations to Deck/Card classes

--- src/pypoker/deck.py
+++ src/pypoker/deck.py
@@ -4,16 +4,132 @@ produces a deck of cards object to be used by the game class
 """
 
 import random
+from dataclasses import dataclass, InitVar, field
+from typing import Union, List
+
+
+@dataclass()
+class Card(object):
+    """
+    Class representing a single playing card
+    """
+
+    rank_value: InitVar[Union[str, int]]
+    suit_value: InitVar[str]
+    rank: str = field(init=False, compare=False, repr=False)
+    suit: str = field(init=False, compare=False, repr=False)
+    value: int = field(init=False, compare=False, repr=False)
+    name: str = field(init=False)
+
+    def __post_init__(self, rank_value, suit_value):
+        self.rank = self._determine_rank(rank_value)
+        self.suit = self._determine_suit(suit_value)
+        self.value = self._determine_value(self.rank)
+        self.name = self._determine_name(self.rank, self.suit)
+
+    def __gt__(self, other):
+        return self.value > other.value
+
+    def __lt__(self, other):
+        return self.value < other.value
+
+    @staticmethod
+    def _determine_suit(suit_value: str) -> str:
+        """
+        determines the suite of the card based on the given value
+
+        :param suit_value: value of the cards suit
+        :return:
+        """
+
+        suit_mapping = {"C": "Clubs", "D": "Diamonds", "H": "Hearts", "S": "Spades"}
+
+        if suit_value not in suit_mapping.keys():
+            raise ValueError(
+                f"Suit value '{suit_value}' is not in list of valid values '{suit_mapping.keys()}"
+            )
+
+        return suit_mapping[suit_value]
+
+    @staticmethod
+    def _determine_rank(rank_value: Union[str, int]) -> str:
+        """
+        returns the english rank of the card based on the value.
+
+        :param rank_value: integer/str representing the value of the card between 2 and A
+        (where 2 represents a 2 and A represents an ace)
+        :return: English rank of the card
+        """
+
+        rank_mapping = {
+            "2": "Two",
+            "3": "Three",
+            "4": "Four",
+            "5": "Five",
+            "6": "Six",
+            "7": "Seven",
+            "8": "Eight",
+            "9": "Nine",
+            "10": "Ten",
+            "J": "Jack",
+            "Q": "Queen",
+            "K": "King",
+            "A": "Ace",
+        }
+
+        if str(rank_value) not in rank_mapping.keys():
+            raise ValueError(
+                f"Specified card rank {rank_value} is not in value mapping dictionary '{rank_mapping}'."
+            )
+
+        return rank_mapping[str(rank_value)]
+
+    @staticmethod
+    def _determine_value(rank: str) -> int:
+        """
+        returns the numeric value of a rank, used for greater_than and less_than comparisons
+
+        :param rank: str representing the rank of a card between "Two" and "Ace"
+        :return: comparison value of a card
+        """
+
+        return {
+            "Two": 2,
+            "Three": 3,
+            "Four": 4,
+            "Five": 5,
+            "Six": 6,
+            "Seven": 7,
+            "Eight": 8,
+            "Nine": 9,
+            "Ten": 10,
+            "Jack": 11,
+            "Queen": 12,
+            "King": 13,
+            "Ace": 14,
+        }[rank]
+
+    @staticmethod
+    def _determine_name(rank: str, suit: str) -> str:
+        """
+        Simple method to return the full english name of a card
+
+        :param rank: the english rank of the card.
+        :param suit: the english suit of the card.
+        :return: Full english name of the card
+        """
+
+        return f"{rank} of {suit}"
 
 
 class Deck(object):
     def __init__(self):
-        self.cards_all = self._build_all_cards()
-        self.cards_available = self.cards_all.copy()
-        self.cards_used = []
+        self.cards_all: List[Card] = self._build_all_cards()
+        self.cards_available: List[Card] = self.cards_all.copy()
+        self.cards_used: List[Card] = []
 
     @staticmethod
-    def _build_all_cards():
+    def _build_all_cards() -> List[Card]:
         """
         Method to populate the self.cards_all attribute
 
@@ -22,13 +138,13 @@ class Deck(object):
 
         cards = []
 
-        for suit in range(4):
-            for value in range(2, 15):
+        for suit in ["H", "D", "C", "S"]:
+            for value in [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"]:
                 cards.append(Card(value, suit))
 
         return cards
 
-    def shuffle(self):
+    def shuffle(self) -> None:
         """
         shuffles the avaliable cards in the deck, reordering self.avaliable_cards object
 
@@ -37,7 +153,7 @@ class Deck(object):
 
         random.shuffle(self.cards_available)
 
-    def draw(self, num=1):
+    def draw(self, num: int = 1) -> List[Card]:
         """
         draws the number of cards from the deck as specified.
 
@@ -71,7 +187,7 @@ class Deck(object):
         self.cards_available = self.cards_all.copy()
 
     @staticmethod
-    def order_cards(cards, descending=False):
+    def order_cards(cards: List[Card], descending: bool = False) -> List[Card]:
         """
         helper method that will order a list of card objects
         :param cards: List of card objects
@@ -93,94 +209,3 @@ class Deck(object):
             ordered_cards.extend(suited_cards)
 
         return ordered_cards
-
-
-class Card(object):
-    """
-    Class representing a single playing card
-    """
-
-    SUIT_MAPPING = {0: "Clubs", 1: "Diamonds", 2: "Hearts", 3: "Spades"}
-    VALUE_MAPPING = {
-        2: "Two",
-        3: "Three",
-        4: "Four",
-        5: "Five",
-        6: "Six",
-        7: "Seven",
-        8: "Eight",
-        9: "Nine",
-        10: "Ten",
-        11: "Jack",
-        12: "Queen",
-        13: "King",
-        14: "Ace",
-    }
-
-    def __init__(self, card_value, suit_value):
-        """
-        the Init method of the card class.
-
-        :param card_value: integer representing the value of the card between 2 and 14
-        (where 2 represents a 2 and 14 represents and ace)
-        :param suit_value: Integer representing the suit of the card between 0 and 3 mapping to the suit of the card
-        (0=Clubs, 1=Diamonds, 2=Hearts, 3=Spades)
-        """
-
-        self.value = card_value
-        self.suit = self._determine_suit(suit_value)
-        self.rank = self._determine_rank(card_value)
-        self.name = self._determine_name(self.rank, self.suit)
-
-    def __eq__(self, other):
-        return self.name == other.name
-
-    def __gt__(self, other):
-        return self.value > other.value
-
-    def __lt__(self, other):
-        return self.value < other.value
-
-    def _determine_suit(self, suit_value):
-        """
-        determines the suite of the card based on the given value
-        (0=Clubs, 1=Diamonds, 2=Hearts, 3=Spades)
-
-        :param suit_value: value of the cards suit
-        :return:
-        """
-
-        if suit_value not in self.SUIT_MAPPING.keys():
-            raise ValueError(
-                f"Suit value '{suit_value}'  is not in list of valid values '{self.SUIT_MAPPING}"
-            )
-
-        return self.SUIT_MAPPING[suit_value]
-
-    def _determine_rank(self, value):
-        """
-        returns the english rank of the card based on the value.
-
-        :param value: integer representing the value of the card between 2 and 14
-        (where 2 represents a 2 and 14 represents and ace)
-        :return: English rank of the card
-        """
-
-        if value not in self.VALUE_MAPPING.keys():
-            raise ValueError(
-                f"Specified card value {value} is not in value mapping dictionary '{self.VALUE_MAPPING}'."
-            )
-
-        return self.VALUE_MAPPING[value]
-
-    @staticmethod
-    def _determine_name(rank, suit):
-        """
-        Simple method to return the full english name of a card
-
-        :param rank: the english rank of the card.
-        :param suit: the english suit of the card.
-        :return: Full english name of the card
-        """
-
-        return f"{rank} of {suit}"
